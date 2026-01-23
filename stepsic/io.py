Implemented gass re-scaling. Fixed parameters in H dependent case. Lbox and Rsim is saved into the hdf5 header. Full resolution is printed out (not just the resolution in z direction)

stepsic/io.py
@@ -96,6 +96,70 @@ class CosmoIO:
         saver(path, data=data, **kwargs)
 
     @staticmethod
+    def get_box_size(path: Path):
+        '''
+        Retrieves the box size from the snapshot file.
+
+        Parameters
+        ----------
+        path : pathlib.Path
+            Path to the snapshot file.
+
+        Returns
+        -------
+        box_size : float
+            The box size in internal units.
+        '''
+        path = path.expanduser().resolve()
+        ext = CosmoIO._get_extension(path)
+        if ext in ['hdf5', 'h5']:
+            with h5py.File(path, 'r') as hdf:
+                box_size = hdf['/Header'].attrs['BoxSize']
+            return box_size
+        elif ext in ['dat', 'txt']:
+            raise NotImplementedError('Box size retrieval from ASCII files is not implemented.')
+        else:
+            if glio is None:
+                raise UnsupportedFormatError(
+                    f'`{ext}` is an unsupported extension and glio is not installed.'
+                )
+            s = glio.GadgetSnapshot(path)
+            box_size = s.header.BoxSize
+            return box_size
+    
+    def get_simulation_radius(path: Path):
+        '''
+        Retrieves the simulation radius from the snapshot file.
+        This will be useful for re-scaling spherical snapshots.
+
+        Parameters
+        ----------
+        path : pathlib.Path
+            Path to the snapshot file.
+
+        Returns
+        -------
+        box_size : float
+            The box size in internal units.
+        '''
+        path = path.expanduser().resolve()
+        ext = CosmoIO._get_extension(path)
+        if ext in ['hdf5', 'h5']:
+            with h5py.File(path, 'r') as hdf:
+                sim_radius = hdf['/Header'].attrs['SimulationRadius']
+            return sim_radius
+        elif ext in ['dat', 'txt']:
+            raise NotImplementedError('Box size retrieval from ASCII files is not implemented.')
+        else:
+            if glio is None:
+                raise UnsupportedFormatError(
+                    f'`{ext}` is an unsupported extension and glio is not installed.'
+                )
+            s = glio.GadgetSnapshot(path)
+            box_size = s.header.BoxSize
+            return box_size / 2.0
+
+    @staticmethod
     def _match_extension(path: Path):
         '''
         Detects the file extension of the given path.
@@ -334,7 +398,8 @@ class CosmoIO:
             h.attrs['MassTable'] = np.zeros(6, dtype=dtype)
             h.attrs['Time'] = 1.0 / (kwargs.get('Redshift', 0) + 1.0)
             h.attrs['Redshift'] = float(kwargs.get('Redshift', 0.0))
-            h.attrs['BoxSize'] = float(kwargs.get('BoxSize', 0.0))
+            h.attrs['BoxSize'] = float(kwargs.get('BoxSize', 0.0)) # only Lz is stored to have compatibility with both T^3 and S^1xR^2 simulations
+            h.attrs['SimulationRadius'] = float(kwargs.get('SimulationRadius', 0.0)) # Rsim, only relevant for R^1xR^2 and R^3 simulations
             h.attrs['NumFilesPerSnapshot'] = kwargs.get('NumFilesPerSnapshot', 1)
             h.attrs['Omega0'] = float(kwargs.get('Omega0', 0.0))
             h.attrs['OmegaLambda'] = float(kwargs.get('OmegaLambda', 0.0))

stepsic/parameters.py
@@ -139,6 +139,7 @@ class CosmoParameters:
         Cosmological Parameters
         -----------------------
         H0:                {self.P['H0']:.3f} km s^-1 Mpc^-1
+        h:                 {self.P['H']:.6f}
         Omega_m:           {self.P['OMEGA_M']:.6f}
         Omega_m h^2:       {self.P['OMMH2']:.6f}
         Omega_c h^2:       {self.P['OMCH2']:.6f}
@@ -176,11 +177,11 @@ class CosmoParameters:
         self._check_scalar('NMESH', dtype=int)
         self._check_array_or_scalar('LBOX', length=3)
         if not self.P['HINDEPENDENT']:
-            self.P['LBOX'] *= self.P['H']
+            self.P['LBOX'] *= self.P['H'] # convert to internal (/h) units
         self._check_array_or_scalar('PERIODIC', length=3, dtype=bool)
         self._check_array_or_scalar('COI', length=3)
         if not self.P['HINDEPENDENT']:
-            self.P['COI'] *= self.P['H']
+            self.P['COI'] *= self.P['H'] # convert to internal (/h) units
         self._check_scalar('LPTORDER', dtype=int)
 
         self._check_scalar('REDSHIFT')
@@ -188,10 +189,10 @@ class CosmoParameters:
 
         self._check_scalar('R_3D')
         if not self.P['HINDEPENDENT']:
-            self.P['R_3D'] *= self.P['H']
+            self.P['R_3D'] *= self.P['H'] # convert to internal (/h) units
         self._check_scalar('D_4D')
         if not self.P['HINDEPENDENT']:
-            self.P['D_4D'] *= self.P['H']
+            self.P['D_4D'] *= self.P['H'] # convert to internal (/h) units
         self._check_scalar('NRBINS', dtype=int)
 
         self._check_string('TYPE')
@@ -222,6 +223,8 @@ class CosmoParameters:
 
         self._check_boolean('SPHEREMODE')
         self._check_boolean('COMOVING')
+        if not self.P['COMOVING'] and self.P['GEOMETRY'] != 'spherical':
+            raise ValueError("Error: non-comoving simulations are only supported in 'spherical' geometry!\nExiting.")
 
         self._check_boolean('COUNTER')
         self._check_scalar('PHASE_SHIFT')
@@ -237,24 +240,46 @@ class CosmoParameters:
         self._check_scalar('UNIT_M_IN_G')
         self._check_scalar('UNIT_V_IN_KMPS')
 
-        text = dedent(f'''
-        IC parameters
-        -------------
-        Random seed:                   {self.P['SEED']:d}
-        Mesh size:                     {self.P['NMESH']} voxels
-        Box size:                      {self.P['LBOX']} {unit}
-        Periodicity along x-y-z axis:  {self.P['PERIODIC']}
-        Target redshift:               {self.P['REDSHIFT']:.3f}
-        Target scale factor:           {self.P['SCALE']:.6f}
-        Center of interest:            {self.P['COI']} {unit}
-        Euclidean simulation radius:   {self.P['R_3D']} {unit}
-        Compact. simulation diameter:  {self.P['D_4D']} {unit}
-        Number of grid samples:        {self.P['NGRIDSAMPLES']:d}
-        Glass input file:              {self.P['INPUT_GLASS']}
-        IC output directory:           {self.P['IC_DIR']}
-        IC name prefix:                {self.P['IC_PREFIX']}
-        Comoving IC:                   {self.P['COMOVING']}
-        Counter phase simulation:      {self.P['COUNTER']}
-        Phase shift:                   {self.P['PHASE_SHIFT']:.2f} degrees
-        ''')
+        if self.P['HINDEPENDENT']:
+            text = dedent(f'''
+            IC parameters
+            -------------
+            Random seed:                   {self.P['SEED']:d}
+            Mesh size:                     {self.P['NMESH']} voxels
+            Box size:                      {self.P['LBOX']} {unit}
+            Periodicity along x-y-z axis:  {self.P['PERIODIC']}
+            Target redshift:               {self.P['REDSHIFT']:.3f}
+            Target scale factor:           {self.P['SCALE']:.6f}
+            Center of interest:            {self.P['COI']} {unit}
+            Euclidean simulation radius:   {self.P['R_3D']} {unit}
+            Compact. simulation diameter:  {self.P['D_4D']} {unit}
+            Number of grid samples:        {self.P['NGRIDSAMPLES']:d}
+            Glass input file:              {self.P['INPUT_GLASS']}
+            IC output directory:           {self.P['IC_DIR']}
+            IC name prefix:                {self.P['IC_PREFIX']}
+            Comoving IC:                   {self.P['COMOVING']}
+            Counter phase simulation:      {self.P['COUNTER']}
+            Phase shift:                   {self.P['PHASE_SHIFT']:.2f} degrees
+            ''')
+        else:
+            text = dedent(f'''
+            IC parameters
+            -------------
+            Random seed:                   {self.P['SEED']:d}
+            Mesh size:                     {self.P['NMESH']} voxels
+            Box size:                      {np.array2string(self.P['LBOX']/self.P['H'], precision=2, floatmode='fixed')} {unit}\t= {np.array2string(self.P['LBOX'], precision=2, floatmode='fixed')} {unit}/h
+            Periodicity along x-y-z axis:  {self.P['PERIODIC']}
+            Target redshift:               {self.P['REDSHIFT']:.3f}
+            Target scale factor:           {self.P['SCALE']:.6f}
+            Center of interest:            {np.array2string(self.P['COI']/self.P['H'], precision=2, floatmode='fixed')} {unit}\t\t= {np.array2string(self.P['COI'], precision=2, floatmode='fixed')} {unit}/h             
+            Euclidean simulation radius:   {self.P['R_3D']/self.P['H']:.4f} {unit}\t\t\t= {self.P['R_3D']:.4f} {unit}/h
+            Compact. simulation diameter:  {self.P['D_4D']/self.P['H']:.4f} {unit}\t\t\t= {self.P['D_4D']:.4f} {unit}/h
+            Number of grid samples:        {self.P['NGRIDSAMPLES']:d}
+            Glass input file:              {self.P['INPUT_GLASS']}
+            IC output directory:           {self.P['IC_DIR']}
+            IC name prefix:                {self.P['IC_PREFIX']}
+            Comoving IC:                   {self.P['COMOVING']}
+            Counter phase simulation:      {self.P['COUNTER']}
+            Phase shift:                   {self.P['PHASE_SHIFT']:.2f} degrees
+            ''')
         log.info(text)